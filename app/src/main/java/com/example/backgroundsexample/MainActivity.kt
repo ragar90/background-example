2364890646f900ad61c854f50d2331c1b4da3e96
Rene Garcia <rene.garciah90@gmail.com>
2020-06-21 19:18:13 -0600

    Adding state drawable for fox btn

--- app/src/main/java/com/example/backgroundsexample/MainActivity.kt
+++ app/src/main/java/com/example/backgroundsexample/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.backgroundsexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+  lateinit var disableFox:Button
+  lateinit var foxBtn:Button
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    disableFox = findViewById(R.id.disableFox)
+    foxBtn = findViewById(R.id.foxBtn)
+    disableFox.setOnClickListener {
+      val state = foxBtn.isEnabled
+      foxBtn.isEnabled = !state
+    }
   }
 }
